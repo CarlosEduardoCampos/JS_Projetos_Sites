62fd1f8b8811fbf67bcab2cb51f7c132c28b15b3
Carlos <carlos.admcode@gmail.com>
2023-02-16 19:51:10 -0300

    Cria funções para calcula o imc e mostra ele no campo de resultado

--- js/imc.js
+++ js/imc.js
@@ -1,10 +1,38 @@
 // Recebe oque esta nos campos de digitação
-let in_nome = document.getElementById(nome).innerText;
-let in_altura = document.getElementById(altura);
-let in_peso = document.getElementById(peso);
+const NOME = document.getElementById('nome');
+const ALTURA = document.getElementById('altura');
+const PESO = document.getElementById('peso');
+const CALCULAR = document.getElementById('calcular');
+const RESULTADO = document.getElementById('resultado');
 
 function calcular()
 {
-    alert("teste");
+    if (campoVazio())
+    {
+        alert('Todos os campos devem ser preenchidos!');
+    }
+    else{
+        let imc = (PESO.value / (ALTURA.value ** 2));
+
+        RESULTADO.innerHTML = '<p><i>'+imc+'</i></p>'
+    }
 }
 
+function campoVazio()
+{
+    if (NOME.value === '') 
+    {
+        return true;
+    }
+    else if(ALTURA.value === '')
+    {
+        return true;
+    }
+    else if(PESO.value === '')
+    {
+        return true;
+    }
+    else{
+        return false;
+    }
+}

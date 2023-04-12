e9192c00be5d13e5a1dbd1ac2d330121d24c2935
Carlos <carlos.admcode@gmail.com>
2023-04-12 20:31:42 -0300

    cria uma imitação do trello

---
+++ sites/trelo/src/scripts.js
@@ -0,0 +1,24 @@
+const PRINCIPAL = document.getElementById('principal');
+let tabela = [
+    {
+        'id':1,
+        'titulo':'tabela1'
+    }
+];
+let itens = [
+    {
+        'refTabela':1,
+        'titulo':'teste00',
+        'descricao':'teste tes te ste e'
+    }
+];
+
+const criarTabela = () => {
+    const tabela = document.createElement('section');
+    tabela.classList.add('tabela');
+
+}
+
+const criarItem = (tabela) => {
+    const item = document.createElement()
+}
